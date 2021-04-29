f5e7733e3252bcab9beaa3f47b3da21834357ee4
Andrew-CC-Martin <amartin262@gmail.com>
2021-04-29 14:56:15 +1000

    start building pokedex

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-export const returnTrue = () => true
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon-species/'
+const pokemonName = document.getElementById("pokemon-name")
+const getPokemonButton = document.getElementById("get-pokemon-button")
+getPokemonButton.addEventListener('click', getPokeName)
+
+function updateNameHeading(name) {
+  pokemonName.innerText = name
+}
+
+async function getPokeName() {
+  pokemonName.innerText = "...loading"
+  let response = await fetch(baseUrl + 1);
+  let data = await response.json()
+  const name = data.name
+  updateNameHeading(name)
+}
+
--- index.test.js
+++
@@ -1,5 +0,0 @@
-import { returnTrue } from './index.js'
-
-test("this is a demo test", () => {
-  expect(returnTrue()).toBe(true)
-})
